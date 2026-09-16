c3143c3ec74956f98d638a58bd934a5e7deb6ab5
dongyeon <bjsealang1@gmail.com>
2026-09-16 17:09:02 +0900

    refactor: remove document domain

--- src/main/java/com/workhelper/domain/document/repository/DocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.workhelper.domain.document.repository;
-
-import com.workhelper.domain.document.entity.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
-}
